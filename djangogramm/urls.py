b069fd930d52c37d8af98a12fdecb4adbb08bf09
Михаил Кузнецов <m.b.kuznetsov@gmail.com>
2021-09-13 01:16:32 +0300

    add accounts/logout

--- djangogramm/urls.py
+++ djangogramm/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import DgUserLoginView
+from .views import DgUserLoginView, profile, DgUserLogoutView
 
 urlpatterns = [
     path('accounts/login/', DgUserLoginView.as_view(), name='login'),
+    path('accounts/logout/', DgUserLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile')
 ]
--- djangogramm/views.py
+++ djangogramm/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 
 
@@ -6,3 +8,12 @@ from django.shortcuts import render
 
 class DgUserLoginView(LoginView):
     template_name = 'djangogramm/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'djangogramm/profile.html')
+
+
+class DgUserLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'djangogramm/logout.html'
